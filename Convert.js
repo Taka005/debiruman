6b4503963e758dc96da68f95d1ea6fcb1ed5c29d
Taka005 <01taka005@gmail.com>
2022-11-20 22:12:32 +0900

    修正

--- Convert.js
+++ Convert.js
@@ -121,8 +121,10 @@ form.addEventListener("submit",async(event)=>{
     console.log(wav2)
     let wav = Convert(wav1,wav2);
     console.log(wav)
-    wav1 = new Uint8Array(wav)
+    wav1 = new Blob([new Uint8Array(wav)],{type:"audio/wav"})
     console.log(wav1)
+    let blob4 = await ReadBlob(blob4);
+    console.log(blob4)
     let blob3 = await ReadFile(text[2]);
     console.log(blob3)
     let wav3 = await ReadBlob(blob3);
